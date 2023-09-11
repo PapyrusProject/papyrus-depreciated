9e9d2f172e370450ccbb46fa9b3f80c1b0865e50
Pedro Cardoso da Silva <pcsilvamaster@gmail.com>
2023-09-11 16:56:25 -0300

    feat:'our mission' session introduction

--- src/components/AboutComponents/AboutCard.tsx
+++ src/components/AboutComponents/AboutCard.tsx
@@ -9,7 +9,7 @@ interface IProps {
 export function AboutCard(props:IProps){
 
   return (
-    <div className="flex flex-col items-center w-1/4 gap-4 px-6 py-20 bg-white rounded-xl">
+    <div className="flex flex-col items-center gap-4 px-6 py-20 bg-white w-96 rounded-xl">
       {props.icon}
       <h2 className="text-2xl font-semibold">{props.title}</h2>
       <p className="text-center">
---
+++ src/components/AboutComponents/SectionOurMission.tsx
@@ -0,0 +1,17 @@
+import Image from "../../Images/rafiki.svg";
+
+export function SectionOurMission(){
+
+  return (
+    <section className="flex items-center justify-center gap-60">
+      <div className="flex flex-col w-1/3 gap-6">
+        <h1 className="text-3xl font-bold text-primary">Our Mission</h1>
+        <p>
+          To empower individuals and teams to manage information, projects, and collaborations more effectively and flexibly. Papyrus aims to provide a versatile and highly customizable platform that enables people to create and organize their own productivity and information management solutions tailored to their unique needs. This includes the creation of documents, databases, to-do lists, notes, and much more, all within a single digital environment.
+        </p>
+      </div>
+      <img src={Image} alt="Imagem de estilo para a pagina" />
+    </section>
+  )
+
+}
