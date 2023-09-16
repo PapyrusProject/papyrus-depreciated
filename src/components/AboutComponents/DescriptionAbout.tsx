bdb0dd7026d847a5696934404771e731cde09dbb
Jonathan Alves <you@example.com>
2023-09-16 18:06:43 -0300

    style: adjust session welcome and add responsivity sesession our mission

--- src/components/AboutComponents/DescriptionAbout.tsx
+++ src/components/AboutComponents/DescriptionAbout.tsx
@@ -1,9 +1,9 @@
 export function DescriptionAbout(){
   return (
     <section
-      className="flex flex-col items-center justify-center w-full px-8 py-16 gap-12 lg:gap-48 lg:flex-row"
+      className="flex flex-col items-center justify-center w-full px-8 py-16 gap-12 lg:gap-48 lg:flex-row lg:px-16"
     >
-      <div className="flex flex-col w-1/2 gap-5 text-center lg:text-left lg:w-1/4">
+      <div className="flex flex-col w-3/4 gap-5 text-center lg:text-left">
         <p className="font-semibold text-primary">
           Welcome to Papyrus
         </p>
@@ -11,7 +11,7 @@ export function DescriptionAbout(){
           Turning Your Inspiration into Expression with Text on Paper
         </h1>
       </div>
-      <div className="w-1/2 text-justify lg:w-1/3">
+      <div className="w-3/4 text-left lg:text-justify">
         <p>
           The Papyrus project is a creative exploration that was born from inspiration in Notion, a powerful digital text editing tool. This project aims to bring the versatility and expressiveness of Notion into the world of paper design, reimagining how ideas are transformed into tangible and inspiring objects.
         </p>
