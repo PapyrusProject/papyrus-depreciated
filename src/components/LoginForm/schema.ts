cfa360b337108778b30baec60562703ffeb1b58d
ggualbertosouza <contatoguilhermegsz@gmail.com>
2023-09-28 21:48:00 -0300

    fix: sql injection preventions

--- src/components/LoginForm/schema.ts
+++ src/components/LoginForm/schema.ts
@@ -1,8 +1,16 @@
 import { z } from 'zod';
 
 export const schema = z.object({
-  email: z.string().email("Endereço de email inválido."),
-  password: z.string().trim().min(1, "A senha é obrigatória.")
+  email: z
+  .string()
+  .email("Endereço de email inválido.")
+  .refine((data) => /[a-z,A-Z,0-9,_]/.test(data.email))
+  ,
+  password: z
+  .string()
+  .trim()
+  .min(1, "A senha é obrigatória.")
+  .refine((data) => /[a-z,A-Z,0-9,_]/.test(data.password))
 })
 
 export type LoginProps = z.infer<typeof schema>;
--- src/components/RegisterForm/schema.ts
+++ src/components/RegisterForm/schema.ts
@@ -2,25 +2,25 @@ import { z } from "zod";
 
 export const schema = z
   .object({
-    username: z.string().min(4, "Usuário precisa ter no mínimo 4 caracteres.").refine((data) => /[^a-z,A-Z,0-9,_]/.test(data),
-    {
-      message: "Usuário deve conter letrar e numeros"
-    }
+    username: z
+    .string()
+    .min(4, "Usuário precisa ter no mínimo 4 caracteres.")
+    .refine((data) => /[^a-z,A-Z,0-9,_]/.test(data.username),
+    {message: "Usuário deve conter letrar e numeros"}
     ),
     email: z
       .string()
       .email("Digite um email válido.")
       .min(2, "Digite o seu e-mail corretamente."),
-    /* .refine(async (data)=> {
+    /* .refine(async (data)=>  {
     // Fazer requisição no DB e verificar se o email já existe.
     return await emailAlreadyExist(data)
   }, 'Esse email já existe.')*/
     password: z
       .string()
-      .min(6, "Sua senha precisa ter no mínimo 6 caracteres.").refine((data) => /[^a-z,A-Z,0-9,_]/.test(data),
-      {
-        message: "Senha deve conter letrar e numeros"
-      }
+      .min(6, "Sua senha precisa ter no mínimo 6 caracteres.")
+      .refine((data) => /[a-z,A-Z,0-9,_]/.test(data.password),
+      {message: "Senha deve conter letrar e numeros"}
       ),
     confirmPassword: z.string().min(6, "Digite a senha correta."),
   })
