229f569e9e5b23411828d02f6d7e0664c88d75cc
ggualbertosouza <contatoguilhermegsz@gmail.com>
2023-10-03 16:51:54 -0300

    fix: Header refatored

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,103 +1,98 @@
-// Pages and components imports
-import Btn from "../Buttons/Btn";
-import Navlink from "../Links/Navlink";
-
-// react router dom packages
-import { useNavigate, useLocation } from "react-router-dom";
-
-//React icons Packages
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+// React Icons
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { PiUserFill } from "react-icons/pi";
 
-//import motion
-import { motion, useCycle } from "framer-motion"
+// React Router Dom
+import { NavLink } from "react-router-dom";
 
-interface HeaderProps {
-  onOpenLoginModal(): void;
-}
+// Pages
+import Btn from "../Buttons/Btn";
 
-type motionProps = {
-  closed: {
-    display: string,
-    y: number,
-  transition: {
-    duration: number,
-  },
-  open: {
-    display: string,
-    y: number,
-    transition: {
-      duration: number,
-    }
-  }
-}}
+// Framer motion
+import { useCycle } from "framer-motion";
+import { _RootProps } from "../../pages/_Root";
 
-export default function Header({ onOpenLoginModal }: HeaderProps) {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const [menu, toggleMenu] = useCycle(false, true)
-
-  function RedirectTo() {
-    navigate("/signup");
-  }
-
-  const HeaderMotion: motionProps = {
-    closed: {
-      display: 'block',
-      y: -500,
-      transition: {
-        duration: 0.5
-      },
-      open: {
-        display: 'none',
-        y: 0,
-        transition: {
-          duration: 0.5
-        }
-      }
-    }
-  }
+export default function Header({ onOpenLoginModal }: _RootProps) {
+  const [menu, toggleMenu] = useCycle(false, true);
 
   return (
-    <>
-      <header 
-      className="flex flex-col items-center justify-around py-2 mx-auto relative">
-        <div className="flex md:hidden absolute left-6 top-4">
-          <Btn
-            icon={menu ? <AiOutlineClose/> : <AiOutlineMenu/>}
-            bgColor="bg-white text-primary"
-            func={toggleMenu}
-          />
+    <div className="md:container md:mx-auto">
+      <header className="flex flex-col md:flex-row items-center justify-between md:py-2">
+        <div className="p-2 flex justify-center">
+          <a href="/" className="text-3xl font-bold">
+            Papy<span className="text-primary">rus.</span>
+          </a>
+          <button
+            className="md:hidden absolute right-4 top-4"
+            onClick={() => toggleMenu()}
+          >
+            {menu ? <AiOutlineClose /> : <AiOutlineMenu />}
+          </button>
         </div>
-        <p className="text-4xl text-primary font-bold">
-          Papy<span className="text-black">rus.</span>
-        </p>
-        <motion.nav 
-        variants={HeaderMotion}
-        animate={menu ? 'open' : 'closed'}
-        className="flex flex-col md:flex-row items-center gap-4 bg-primary w-full md:bg-white mt-2 py-2 md:py-0">
-          <Navlink name="Home" page="/" atualPage={location.pathname === "/"}/>
-          <Navlink name="About" page="/about" atualPage={location.pathname === "/about"} />
-          <Navlink name="Service" page="/service" atualPage={location.pathname === "/service"} />
-          <Navlink name="Contact" page="/contact" atualPage={location.pathname === "/contact"} />
-        <div className="flex gap-6">
+
+        <nav
+          className={`flex md:flex-row items-center gap-6 bg-primary mt-4 pt-4 md:mt-0 md:pt-0 md:bg-white w-full md:w-fit ${
+            menu ? "flex-col" : "hidden md:flex"
+          }`}
+        >
+          <NavLink
+            to={"/"}
+            className={({ isActive }) =>
+              `font-bold ${
+                isActive ? "text-white md:text-secondary" : "text-black"
+              }`
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to={"/about"}
+            className={({ isActive }) =>
+              `font-bold ${
+                isActive ? "text-white md:text-secondary" : "text-black"
+              }`
+            }
+          >
+            About
+          </NavLink>
+          <NavLink
+            to={"/service"}
+            className={({ isActive }) =>
+              `font-bold ${
+                isActive ? "text-white md:text-secondary" : "text-black"
+              }`
+            }
+          >
+            Service
+          </NavLink>
+          <NavLink
+            to={"/contact"}
+            className={({ isActive }) =>
+              `font-bold ${
+                isActive ? "text-white md:text-secondary" : "text-black"
+              }`
+            }
+          >
+            Contact
+          </NavLink>
+        </nav>
+        <div
+          className={`flex justify-center gap-6 bg-primary mb-4 py-4 md:mb-0 md:py-0 md:bg-white w-full md:w-fit ${
+            menu ? "" : "hidden md:flex"
+          }`}
+        >
           <Btn
-            icon={<PiUserFill />}
             name="Sign Up"
-            bgColor="bg-primary text-white items-center"
-            hover="hover:bg-white hover:text-primary"
-            atualPage={location.pathname === "/signup"}
-            func={RedirectTo}
+            icon={<PiUserFill />}
+            bgColor="bg-primary text-white hover:bg-white hover:text-primary"
           />
           <Btn
             name="Login"
-            bgColor="bg-white text-primary"
-            hover="hover:bg-primary hover:text-white"
+            bgColor="bg-white text-primary hover:bg-primary hover:text-white"
             func={onOpenLoginModal}
           />
         </div>
-        </motion.nav>
       </header>
-    </>
+    </div>
   );
 }
